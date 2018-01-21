1035b876c821ad85635d17bc6e722bcf928c2af8
acakluqman <luqmanhakem922@gmail.com>
2018-01-21 13:02:03 +0700

    Update exclude.js

--- exclude.js
+++ exclude.js
@@ -1,11 +1,5 @@
-<script type='text/javascript'>
-//<![CDATA[
-protected_links = "nontonanime18.blogspot.co.id,facebook.com,google.com";
-  (function() {
-    var sl = document.createElement('script');
-    sl.type = 'text/javascript'; sl.async = true;
-    sl.src = 'https://cdn.rawgit.com/acakluqman/SafeLink/5c1e951e/kompisafelink2.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl);
-  })();
-//]]>
+<script type="text/javascript" src="https://cdn.rawgit.com/acakluqman/SafeLink/5c1e951e/kompisafelink2.js"></script>
+<script type="text/javascript">
+protected_links = "nontonanime18.blogspot.com";
+auto_safelink();
 </script>
