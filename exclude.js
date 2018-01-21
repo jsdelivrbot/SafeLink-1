b67c8dbafb7b71b82f35b6235cac0703cf142e16
acakluqman <luqmanhakem922@gmail.com>
2018-01-21 13:02:42 +0700

    Update exclude.js

--- exclude.js
+++ exclude.js
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="https://cdn.rawgit.com/acakluqman/SafeLink/5c1e951e/kompisafelink2.js"></script>
 <script type="text/javascript">
-protected_links = "nontonanime18.blogspot.com";
+protected_links = "https://link-nontonanime18.blogspot.com/p/go-to-url.html";
 auto_safelink();
 </script>
