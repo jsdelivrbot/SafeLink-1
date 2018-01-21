992c75044f84873f8ab261e48f60284f1afada68
acakluqman <luqmanhakem922@gmail.com>
2018-01-21 12:49:21 +0700

    Create exclude.js

---
+++ exclude.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'>
+//<![CDATA[
+protected_links = "nontonanime18.blogspot.co.id,facebook.com,google.com";
+  (function() {
+    var sl = document.createElement('script');
+    sl.type = 'text/javascript'; sl.async = true;
+    sl.src = 'https://cdn.rawgit.com/acakluqman/SafeLink/5c1e951e/kompisafelink2.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl);
+  })();
+//]]>
+</script>
